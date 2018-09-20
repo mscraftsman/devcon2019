478d64034b26b2df84d1c6b2c033b77a57b935d1
Cedrik1712 <cedricpoilly@outlook.com>
2018-09-20 19:14:40 +0400

    TEST: use data from files via raw.githubusercontent.com

--- web/app/src/App.vue
+++ web/app/src/App.vue
@@ -7,6 +7,18 @@
     <router-view/>
   </div>
 </template>
+<script>
+export default {
+  created() {
+    fetch(
+      "https://raw.githubusercontent.com/mscraftsman/devcon2019/master/web/app/package.json"
+    )
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+};
+</script>
+
 
 <style lang="scss">
 * {

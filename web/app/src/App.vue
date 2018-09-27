0a6d88f837e8da85075ad3d3c731d406150be196
Cedrik1712 <cedricpoilly@outlook.com>
2018-09-27 20:53:27 +0400

    WIP demo: display text from external sources

--- web/app/src/App.vue
+++ web/app/src/App.vue
@@ -8,15 +8,7 @@
   </div>
 </template>
 <script>
-export default {
-  created() {
-    fetch(
-      "https://raw.githubusercontent.com/mscraftsman/devcon2019/master/web/app/package.json"
-    )
-      .then(response => response.json())
-      .then(data => console.log(data));
-  }
-};
+export default {};
 </script>
 
 
--- web/app/src/components/HelloWorld.vue
+++ web/app/src/components/HelloWorld.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <p>Package name: {{ name }}</p>
+    <p>Package version: {{ version }}</p>
   </div>
 </template>
 
 <script>
+import { FETCH_HOME_TEXT_CONTENT } from "@/constants/action-types";
+
 export default {
   name: "HelloWorld",
+
   props: {
     msg: String
+  },
+
+  data() {
+    return {
+      name: "",
+      version: ""
+    };
+  },
+
+  created() {
+    this.$store.dispatch(FETCH_HOME_TEXT_CONTENT).then(() => {
+      this.name = this.$store.state.homeTextContent.name;
+      this.version = this.$store.state.homeTextContent.version;
+    });
   }
 };
 </script>

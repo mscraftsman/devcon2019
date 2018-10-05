5bbb85889b3c8989dba325f4eb99a7900193644b
Jochen Kirstätter <joki@ios.mu>
2018-10-05 18:05:39 +0400

    add link to CfP

--- web/app/src/components/HelloWorld.vue
+++ web/app/src/components/HelloWorld.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="hello">
     <div class="announcement">
-      <img src="@/assets/not-text.png" alt="">
+      <img src="@/assets/not-text.png" alt="Developers Conference 2019 - Mauritius - 11. - 13. April 2019">
     </div>
-      <a href="https://github.com/mscraftsman/devcon2019/raw/master/assets/DevCon2019-SponsorshipProposal.pdf"
-        download class="sponsor-button"><img src="@/assets/btn_download-brochure.png" alt=""></a>
-    </div>
+    <a href="https://github.com/mscraftsman/devcon2019/raw/master/assets/DevCon2019-SponsorshipProposal.pdf"
+      download class="sponsor-button"><img src="@/assets/btn_download-brochure.png" alt=""></a>
+    <a href="https://sessionize.com/devcon-mauritius-2019/"
+      download class="cfp-button"><img src="@/assets/btn_callforpaper.png" alt=""></a>
+  </div>
 </template>
 
 <script>
@@ -28,10 +30,9 @@ export default {
     padding-top: 10vh;
   }
 }
-.sponsor-button {
+.sponsor-button, .cfp-button {
   position: absolute;
   bottom: 10vh;
-  left: 50%;
   transform: translateX(-50%);
   padding: 20px 30px;
   font-size: 20px;
@@ -50,10 +51,16 @@ export default {
     transition: all 0.2s ease-out;
   }
 }
+.sponsor-button {
+  left: 40%;
+}
+.cfp-button {
+  left: 60%;
+}
 
 @media screen and (max-width: 728px) {
-  .sponsor-button {
-    bottom: 10vh;
+  .sponsor-button, .cfp-button {
+    left: 50%;
     padding: 15px;
     font-size: 15px;
 
@@ -61,6 +68,12 @@ export default {
       padding-top: 10px;
       font-size: 9px;
     }
+  }
+  .sponsor-button {
+    bottom: 30vh;
+  }
+  .cfp-button {
+    bottom: 10vh;
   }
 }
 h3 {

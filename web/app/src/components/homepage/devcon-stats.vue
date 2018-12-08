932a50f1501f2e288e9296d2ba3e3dd522047d18
Sandeep Ramgolam <sandeep@lsl.digital>
2018-12-08 15:17:24 +0400

    speaker-stats

--- web/app/src/components/homepage/devcon-stats.vue
+++ web/app/src/components/homepage/devcon-stats.vue
@@ -1,3 +1,109 @@
 <template>
-  <div class="home-component devcon-stats-wrapper">devcon-stats</div>
+  <div class="home-component devcon-stats-wrapper">
+    <div class="container">
+      <div class="devcon-stats-container">
+        <div class="stat-wrapper">
+          <div class="number">195</div>
+          <div class="label">Speakers</div>
+        </div>
+        <div class="stat-wrapper">
+          <div class="number">195</div>
+          <div class="label">Speakers</div>
+        </div>
+        <div class="stat-wrapper">
+          <div class="number">+570</div>
+          <div class="label">Sponsors</div>
+        </div>
+        <div class="stat-wrapper">
+          <div class="number">+195</div>
+          <div class="label">Speakers</div>
+        </div>
+        <div class="stat-wrapper">
+          <div class="number">195</div>
+          <div class="label">Speakers</div>
+        </div>
+        <div class="stat-wrapper">
+          <div class="number">195</div>
+          <div class="label">Speakers</div>
+        </div>
+        <div class="stat-wrapper">
+          <div class="number">+570</div>
+          <div class="label">Sponsors</div>
+        </div>
+        <div class="stat-wrapper">
+          <div class="number">+195</div>
+          <div class="label">Speakers</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+
+<style lang="scss" scoped>
+.devcon-stats-wrapper {
+  padding: 100px 0;
+}
+.devcon-stats-container {
+  display: grid;
+  // justify-content: space-around;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-row-gap: 100px;
+
+  .stat-wrapper {
+    position: relative;
+    padding: 30px 0 0 0;
+    --width: 200px;
+    width: var(--width);
+
+    &:before {
+      content: " ";
+      width: var(--width);
+      height: 140px;
+      // left: 50%;
+      top: 0px;
+      left: 0;
+      margin-left: -calc(var(--width) / 2);
+
+      position: absolute;
+      background-image: url("../../assets/menu-hover.svg");
+      background-repeat: no-repeat;
+      background-position: center center;
+      background-size: contain;
+      transform: translate(0px, 10px);
+      opacity: 0.5;
+      transition: all 0.5s ease-out;
+    }
+    &:after {
+      content: " ";
+      --width: 100px;
+      width: var(--width);
+      height: 40px;
+      left: calc(var(--width) * 1.2);
+      top: 0px;
+      // left: 0;
+      // margin-left: -calc(var(--width) / 2);
+
+      position: absolute;
+      background-image: url("../../assets/menu-hover.svg");
+      background-repeat: no-repeat;
+      background-position: center center;
+      background-size: contain;
+      transform: translate(0px, 10px);
+      opacity: 0.5;
+      transition: all 0.5s ease-out;
+    }
+
+    .number {
+      font-size: 60px;
+      font-weight: 900;
+      text-align: center;
+    }
+    .label {
+      text-align: center;
+      font-size: 21px;
+      text-transform: uppercase;
+    }
+  }
+}
+</style>
--- web/app/src/components/homepage/five-years.vue
+++ web/app/src/components/homepage/five-years.vue
@@ -1,3 +1,31 @@
 <template>
-  <div class="home-component five-years-wrapper">five-years</div>
+  <div class="home-component five-years-wrapper">
+    <div class="five-years-container">
+      <div class="fiveyr">
+        <img src="../../assets/5-years-devcon.png" alt>
+      </div>
+
+      <div class="tribute-text">
+        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.five-years-container {
+}
+.fiveyr {
+  text-align: center;
+  padding: 100px 0 50px;
+}
+
+.tribute-text {
+  width: 640px;
+  max-width: 80%;
+  margin: 0 auto;
+  text-align: center;
+  line-height: 36px;
+}
+</style>
+

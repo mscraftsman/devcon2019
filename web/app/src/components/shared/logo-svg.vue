3c644bf9d85b541020e38cb69e4a90f83a117daf
Sandeep Ramgolam <sandeep@lsl.digital>
2018-12-08 14:46:45 +0400

    header stripe

---
+++ web/app/src/components/shared/logo-svg.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="logo-wrapper">
+    <img src="@/assets/logo.svg" alt>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.logo-wrapper {
+  width: 80px;
+  height: 80px;
+}
+</style>
---
+++ web/app/src/components/shared/main-menu.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="main-menu-wrapper">
+    <ul>
+      <li>
+        <a href="#">Sessions</a>
+      </li>
+      <li>
+        <a href="#">Extras</a>
+      </li>
+      <li>
+        <a href="#">Life at devcon</a>
+      </li>
+      <li>
+        <a href="#">MCB Innovapp</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+
+<style lang="scss">
+.main-menu-wrapper {
+  ul {
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: space-around;
+    li {
+      list-style: none;
+      a {
+        font-weight: 900;
+        text-transform: uppercase;
+        line-height: 80px;
+        display: block;
+        position: relative;
+        text-decoration: none;
+
+        &:before {
+          content: " ";
+          width: 70px;
+          height: 80px;
+          left: 50%;
+          margin-left: -35px;
+
+          position: absolute;
+          background-image: url("../../assets/menu-hover.svg");
+          background-repeat: no-repeat;
+          background-position: center center;
+          transform: translate(0px, 10px);
+          opacity: 0;
+          transition: all 0.5s ease-out;
+        }
+
+        &:hover,
+        &:active {
+          color: $color-red-light;
+          transition: color 0.2s ease-in-out;
+
+          &:before {
+            opacity: 1;
+            transform: translate(0, 0);
+            transition: all 0.5s ease-in-out;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
+

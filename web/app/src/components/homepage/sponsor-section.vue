e735c5b0229d2eeb3efcca2e777b0f115ecd90ac
Sandeep Ramgolam <sandeep@lsl.digital>
2019-01-02 13:04:39 +0400

    added sponsors style

--- web/app/src/components/homepage/sponsor-section.vue
+++ web/app/src/components/homepage/sponsor-section.vue
@@ -12,17 +12,109 @@
         </a>
       </div>
 
-      <a href="#" class="view-more">View all sponsors</a>
+      <h4 class="subtitle">Gold</h4>
+      <div class="sponsors-wrapper gold-wrapper">
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/mcb.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/sdworx.png" alt>
+        </a>
+      </div>
 
-      <div class="tribute-text">Lorem ipsum dolor sit amet consectetur
+      <h4 class="subtitle">Speaker Sponsor</h4>
+      <div class="sponsors-wrapper medium-wrapper">
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/mcb.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/sdworx.png" alt>
+        </a>
+      </div>
+      <h4 class="subtitle">Silver</h4>
+      <div class="sponsors-wrapper medium-wrapper">
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/mcb.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/sdworx.png" alt>
+        </a>
+      </div>
+      <h4 class="subtitle">Bronze</h4>
+      <div class="sponsors-wrapper small-wrapper">
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/mcb.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/sdworx.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/mcb.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/sdworx.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/mcb.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/sdworx.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/mcb.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/sdworx.png" alt>
+        </a>
+      </div>
+      <h4 class="subtitle">Media Partner</h4>
+      <div class="sponsors-wrapper small-wrapper">
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/mcb.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/sdworx.png" alt>
+        </a>
+      </div>
+      <h4 class="subtitle">Happy Hour</h4>
+      <div class="sponsors-wrapper small-wrapper">
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/mcb.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/sdworx.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/mcb.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/sdworx.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/mcb.png" alt>
+        </a>
+        <a href="#" class="sponsor-logo">
+          <img src="../../assets/sample-sponsors/sdworx.png" alt>
+        </a>
+      </div>
+
+      <div class="tribute-text">
+        Lorem ipsum dolor sit amet consectetur
         adipisicing elit. Aliquam ea itaque magni, omnis cum reprehenderit
         possimus laudantium, doloribus veritatis dolorem repellat explicabo
-        odio quas architecto nobis error nisi. Molestiae, sunt.</div>
+        odio quas architecto nobis error nisi. Molestiae, sunt.
+      </div>
     </div>
 
     <div class="button-center">
-      <button-waw :theme="'#97d0e1'" :size="'80px'" :link="'https://github.com/mscraftsman/devcon2019/raw/master/assets/DevCon2019-SponsorshipProposal.pdf'">Become
-        a Sponsor</button-waw>
+      <button-waw
+        :theme="'#97d0e1'"
+        :size="'80px'"
+        :link="'https://github.com/mscraftsman/devcon2019/raw/master/assets/DevCon2019-SponsorshipProposal.pdf'"
+      >
+        Become
+        a Sponsor
+      </button-waw>
     </div>
   </div>
 </template>
@@ -51,26 +143,94 @@ export default {
 .home-component {
   padding: $section-spacer 0;
 }
+
+.mega-rainbow {
+  padding-bottom: 30px;
+}
+
 .sponsors-wrapper {
-  display: grid;
-  padding: 50px 0;
-  border-bottom: 1px solid rgba(#fff, 0.3);
+  display: flex;
+  flex-wrap: wrap;
+  padding: 20px 0 50px;
+  justify-content: center;
+  align-items: center;
+
+  &:last-child {
+    border-bottom: 1px solid rgba(#fff, 0.3);
+  }
 
   .sponsor-logo {
+    padding: 10px;
+
+    &:last-child {
+      padding: 5px;
+    }
+    display: block;
     img {
       display: block;
       width: 250px;
       height: auto;
-      margin: 0 auto;
+      padding: 10px;
+      // margin: 0 auto;
     }
   }
 }
 .platinum-wrapper {
-  grid-template-columns: 1fr 1fr;
+  // grid-template-columns: 1fr 1fr;
 
   @media screen and (max-width: $tablet) {
-    grid-template-columns: 1fr;
-    grid-row-gap: 40px;
+    // grid-template-columns: 1fr;
+    // grid-row-gap: 40px;
+  }
+}
+
+.gold-wrapper {
+  // grid-template-columns: 1fr 1fr;
+
+  @media screen and (max-width: $tablet) {
+    // grid-template-columns: 1fr;
+    // grid-row-gap: 40px;
+  }
+}
+.medium-wrapper {
+  .sponsor-logo {
+    img {
+      width: 200px;
+    }
+  }
+
+  @media screen and (max-width: $tablet) {
+    .sponsor-logo {
+      &:nth-child(even) {
+        padding: 5px;
+      }
+      img {
+        width: 150px;
+      }
+    }
+  }
+}
+.small-wrapper {
+  .sponsor-logo {
+    img {
+      width: 150px;
+    }
+  }
+  @media screen and (max-width: $tablet) {
+    .sponsor-logo {
+      &:nth-child(even) {
+        padding: 5px;
+      }
+      img {
+        width: 100px;
+      }
+    }
+  }
+}
+
+.sponsor-section-wrapper {
+  .subtitle {
+    padding-top: 20px;
   }
 }
 

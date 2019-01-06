45c2a7ddecd7ac76dab34ce7a6cbea5613f4325c
Arwin Neil Baichoo <arwinneil@gmail.com>
2019-01-06 15:51:45 +0400

    Update Button
    Animation, Font Weight, Padding for proportion

--- web/app/src/components/shared/button-waw.vue
+++ web/app/src/components/shared/button-waw.vue
@@ -24,35 +24,27 @@ export default {
 };
 </script>
 
-
-
 <style lang="scss" scoped>
+
 .btn {
-  --theme: #ff4932;
+ --theme: #ff4932;
   color: var(--theme);
   cursor: pointer;
-  font-size: 16px;
-  font-weight: 400;
+  font-size: 18px;
+  font-weight: 600;
   position: relative;
   text-decoration: none;
   line-height: var(--size, 45px);
   text-transform: uppercase;
-  // width: 100%;
   text-align: center;
-
-  border: 0 solid;
-  box-shadow: inset 0 0 20px rgba(255, 255, 255, 0);
-  outline: 1px solid;
-  outline-color: var(--theme);
-  outline-offset: 0px;
-  // border-radius: 5px;
-  text-shadow: none;
-  transition: all 1250ms cubic-bezier(0.19, 1, 0.22, 1);
+  border-radius: 15px;
+  border: 1px solid var(--theme);
+  transition: 0.25s;
 
   &:hover {
     text-decoration: none;
   }
-  padding: 0 20px;
+  padding: 0px 60px;
 
   a,
   a span {
@@ -60,7 +52,6 @@ export default {
   }
 
   &.effect-hover {
-    // border: 1px solid var(--theme);
     overflow: hidden;
     position: relative;
 
@@ -93,15 +84,17 @@ export default {
   /////////////////////////////
   // Click
   ///////////////////////////
-
+ 
   &.click:active,
-  &.click:focus {
-    border: 1px solid;
-    box-shadow: inset 0 0 20px rgba(255, 255, 255, 0.5),
-      0 0 20px rgba(255, 255, 255, 0.2);
-    outline-color: rgba(255, 255, 255, 0);
-    outline-offset: 15px;
-    text-shadow: 1px 1px 2px #427388;
+  &.click:focus{
+
+  animation: pulse 0.3s;
+  box-shadow: 0 0 0 1em rgba(black,0);
+
+  @keyframes pulse {
+  0% { box-shadow: 0 0 0 0 var(--theme); }
+  }
+  
   }
 }
 </style>

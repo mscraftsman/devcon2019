afa245dbee132a66d144fc0b17a3aba7ee758aff
Jochen Kirstätter <jochenkirstaetter@users.noreply.github.com>
2019-02-20 20:41:01 +0400

    ⚡ update deployment

--- web/app/dist/service-worker.js
+++ web/app/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.061c2c2feba5b364e6e953b26cc438a5.js"
+  "/precache-manifest.76882b1539b59af888763c2050513319.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "devcon2019"});

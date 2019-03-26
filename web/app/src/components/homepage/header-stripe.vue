542c52c6b2654db621eb3e491f95ca5ef148c906
Kushul Soomaree <kushul@lsl.digital>
2019-03-26 09:41:06 +0400

    menu responsive

--- web/app/src/components/homepage/header-stripe.vue
+++ web/app/src/components/homepage/header-stripe.vue
@@ -2,19 +2,17 @@
   <div class="home-component header-stripe-wrapper">
     <div class="container header-stripe-container">
       <div class="logo-wrapper">
-        <LogoSvg/>
+        <LogoSvg />
       </div>
-      <div class="menu-wrapper">
-        <MainMenu/>
+      <div class="menu-wrapper" :class="{ active }">
+        <MainMenu @click.native="resetMenu()" />
       </div>
-      <div class="user" v-if="user.status">{{ user.data.name }}</div>
-      <div class="register-button" v-else>
-        <button-waw
-          :theme="'#ff4932'"
-          :target="'_blank'"
-          :link="'https://www.meetup.com/MauritiusSoftwareCraftsmanshipCommunity/events/258586831/'"
-        >Register</button-waw>
+      <div class="register-button">
+        <button-waw :theme="'#ff4932'" :target="'_blank'" :link="'https://www.meetup.com/MauritiusSoftwareCraftsmanshipCommunity/events/258586831/'">Register</button-waw>
       </div>
+      <button class="menu-button" @click="activateMenu()" :class="{ active }">
+        <i></i>
+      </button>
     </div>
   </div>
 </template>
@@ -22,18 +20,31 @@
 <script>
 import LogoSvg from "@/components/shared/logo-svg.vue";
 import MainMenu from "@/components/shared/main-menu.vue";
-import { mapGetters } from "vuex";
 // import ButtonWaw from '@/components/shared/button-waw.vue'
 
 export default {
   name: "home",
+  data() {
+    return {
+      active: "",
+    };
+  },
   components: {
     LogoSvg,
     MainMenu,
     // ButtonWaw
   },
-  computed: {
-    ...mapGetters({ user: "getUser" }),
+  methods: {
+    activateMenu() {
+      if (this.active === "") {
+        this.active = "active";
+      } else {
+        this.active = "";
+      }
+    },
+    resetMenu() {
+      this.active = "";
+    },
   },
 };
 </script>
@@ -54,19 +65,87 @@ export default {
 .register-button {
 }
 
-.user {
-  // border: 1px solid var(--color-red-light);
-  // padding: 5px;
-  // border-radius: 5px;
-}
-@media screen and (max-width: $tablet) {
-  .logo-wrapper {
+.menu-button {
+  display: none;
+  background: transparent;
+  border: 0;
+  width: 100%;
+  outline: none;
+  cursor: pointer;
+  padding: 0;
+
+  i {
+    position: relative;
+    display: block;
+    text-align: center;
+    width: 40px;
+    height: 3px;
+    color: var(--color-white);
+    background: var(--color-white);
+    transition: all 0.2s ease-out;
+
+    &:after,
+    &:before {
+      content: "";
+      width: 40px;
+      height: 3px;
+      background: var(--color-white);
+      position: absolute;
+      left: 0;
+      transition: all 0.3s ease-in-out;
+    }
+    &:after {
+      bottom: -11px;
+    }
+    &:before {
+      top: -11px;
+    }
   }
+
+  &.active {
+    i {
+      background: transparent;
+      &:after {
+        bottom: 0;
+        transform: rotate(-45deg);
+        opacity: 1;
+      }
+      &:before {
+        top: 0;
+        transform: rotate(45deg);
+        opacity: 1;
+      }
+    }
+  }
+}
+
+@media screen and (max-width: $tablet) {
   .menu-wrapper {
-    display: none;
+    position: absolute;
+    width: 250px;
+    transform: translateX(-250px);
+    transition: all 0.3s ease-in-out;
+    top: 0;
+    left: 0;
+    z-index: 10000000;
+
+    &.active {
+      transform: translateX(0px);
+    }
   }
   .register-button {
     display: none;
+  }
+  .menu-button {
+    height: 100%;
+    justify-content: center;
+    display: flex !important;
+    justify-content: center;
+    align-items: center;
+  }
+  .header-stripe-container {
+    display: grid;
+    grid-template-columns: 1fr 40px;
   }
 }
 </style>

578f53c6b95d8a6ad54a2edb6772cd7562b6bc92
Sandeep Ramgolam <sandeep.ramgolam@gmail.com>
2019-04-09 20:13:32 +0400

    build

--- web/app/dist/service-worker.js
+++ web/app/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.49de4cb407870abbf01d26d43e9f5d4e.js"
+  "/precache-manifest.6aee1c5d24e74cd8d329712bc3458c5c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "devcon2019"});
